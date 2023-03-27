d40f20f36aa1a3ac742d93b741db8e0facda2e11
Talha Imran <talhaimran284@gmail.com>
2023-03-27 22:30:41 +0500

    Reverted profile image rendering

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,34 +9,22 @@ import HeroText from "components/HeroText";
 import { useScreenSize } from "utils/useWindowDimensions";
 
 const HeroSection = () => {
-  const [isMobile, isTab, isDesktop] = useScreenSize();
+  const [isMobile, isTab] = useScreenSize();
 
   return (
     <div className="max-w-[1110px] mx-auto relative md:h-[600px] lg:h-[720px]">
-      {isMobile && (
-        <Image
-          src={ProfileImageMob}
-          className="absolute top-0 right-2/4 translate-x-2/4 w-[174px]"
-          alt="Profile Image"
-          placeholder="blur"
-        />
-      )}
-
-      {isTab && (
-        <Image
-          src={ProfileImageTablet}
-          className="absolute top-0 right-0 w-[322px] h-full"
-          alt="Profile Image"
-          placeholder="blur"
-        />
-      )}
-
-      {isDesktop && (<Image
-        src={ProfileImageDesktop}
-        className="absolute top-0 right-0 w-[445px] h-full"
+      <Image
+        src={
+          isMobile
+            ? ProfileImageMob
+            : isTab
+            ? ProfileImageTablet
+            : ProfileImageDesktop
+        }
+        className="absolute top-0 right-2/4 md:right-0 translate-x-2/4 md:translate-x-0 w-[174px] md:w-[322px] lg:w-[445px] md:h-full"
         alt="Profile Image"
         placeholder="blur"
-      />)}
+      />
       <Navbar />
       <HeroText />
     </div>
